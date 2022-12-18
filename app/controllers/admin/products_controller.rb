67d48246d31171af0501cd2e0f97ef355ec25f12
rodrigocezzar <rodrigocezzar85@gmail.com>
2022-12-18 00:38:25 -0300

    create settings for edit user

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -46,7 +46,7 @@ module Admin
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :publish, :image, :category_id)
+      params.require(:product).permit(:name, :description, :price, :publish, :image, :category_id, :promo, :promo_price)
     end
   end
 end
